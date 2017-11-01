e16cfd06d8f20893fe0d35084382c6383508dd2d
Falco Prescher <falcoprescher@falcoprescher@hotmail.com>
2017-11-01 02:39:52 +0100

    Adding screen script

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 docker volume create starbound
 
 docker run -it --name starbound \
     --mount "source=starbound,target=/starbound/data" \
     -p 21025:21025 -p 21025:21025/udp -p 21026:21026 -p 21026:21026/udp \
-    # -e "STEAM_USERNAME=your_username" -e "STEAM_PASSWORD=your_password" \
+    -e "STEAM_USERNAME=anonymous" -e "STEAM_PASSWORD=" \
     falcoprescher/starbound:1.0
