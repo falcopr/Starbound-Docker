3871fb5c969b93f8ece53f1971174914fa547bfd
Falco Prescher <falcoprescher@falcoprescher@hotmail.com>
2017-11-11 22:52:05 +0100

    Stopping container before killing the screen

--- cron_update.sh
+++ cron_update.sh
@@ -1,6 +1,6 @@
 cd "${0%/*}"
-screen kill starbound
 docker stop starbound
+screen kill starbound
 ./backup.sh
 docker run --rm -v starbound:/starbound/data --entrypoint ./scripts/update_fur.sh falcoprescher/starbound:1.1
 ./start.screen.sh
