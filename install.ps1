088f370243cef13d68ef77a7ea49c37bfa5bb18c
Falco Prescher <falcoprescher@falcoprescher@hotmail.com>
2017-11-11 21:31:02 +0100

    Correcting tag number

--- install.ps1
+++ install.ps1
@@ -6,7 +6,7 @@ docker run -it --name starbound_install `
     --mount "source=starbound,target=/starbound/data" `
     -p 21025:21025 -p 21025:21025/udp -p 21026:21026 -p 21026:21026/udp `
     -e "STEAM_USERNAME=anonymous" -e "STEAM_PASSWORD=" `
-    falcoprescher/starbound:1.0 `
+    falcoprescher/starbound:1.1 `
     -i
 
 
--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ docker run -it --name starbound \
     --mount "source=starbound,target=/starbound/data" \
     -p 21025:21025 -p 21025:21025/udp -p 21026:21026 -p 21026:21026/udp \
     -e "STEAM_USERNAME=anonymous" -e "STEAM_PASSWORD=" \
-    falcoprescher/starbound:1.0 \
+    falcoprescher/starbound:1.1 \
     -s
