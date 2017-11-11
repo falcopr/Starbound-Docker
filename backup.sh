ca7a71860a4379c2881fba3d827a7d36637fa0d3
Falco Prescher <falcoprescher@falcoprescher@hotmail.com>
2017-11-11 23:42:22 +0100

    Naming backup container

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -d -v starbound:/volume -v "$(pwd)/backup:/backup" debian:buster \
+docker run --rm -d --name starbound_backup -v starbound:/volume -v "$(pwd)/backup:/backup" debian:buster \
     tar -czf /backup/starbound-$(date +%Y-%m-%d_%H-%M-%S).tar.gz -C /volume ./
